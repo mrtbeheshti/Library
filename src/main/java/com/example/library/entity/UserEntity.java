d41153dbfbda6e9ca4c6478050c69bf29b4a4c94
Mohammad Reza Tashakori <mrtbeheshti@gmail.com>
2023-04-20 14:48:18 +0430

    FEATURE-9. UserEntity implemented.

---
+++ src/main/java/com/example/library/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package com.example.library.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity extends BaseEntity{
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column
+    private LocalDate birthdate;
+
+    @Column
+    private int reserves;
+
+}
